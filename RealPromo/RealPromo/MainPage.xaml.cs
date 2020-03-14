487da219de4dfe5721032186cfa1939edd627c4a
alissonbeloti <alisson.beloti@gmail.com>
2020-03-14 19:05:32 -0300

    Xamarin quase finalizado

---
+++ RealPromo/RealPromo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealPromo.Models;
+using Xamarin.Forms;
+
+namespace RealPromo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<Promocao> promocoes;
+        public MainPage()
+        {
+            InitializeComponent();
+            ListaPromocoes();
+            ListViewPromocao.ItemsSource = promocoes;
+            Device.StartTimer(TimeSpan.FromSeconds(15), () => {
+                promocoes.Add(new Promocao { Chamada = "simulando observable", NomeEmpresa = "Observador", Regras = "2 unidades", Url = "http://www.carrefour.com.br" });
+                return true;
+            });
+        }
+        private void ListaPromocoes()
+        {
+            promocoes = new ObservableCollection<Promocao>()
+            {
+                new Promocao {Chamada = "TVs 4K em promoção", NomeEmpresa = "Carrefour", Regras= "2 unidades", Url = "http://www.carrefour.com.br"},
+                new Promocao {Chamada = "TVs 4K OLED em promoção", NomeEmpresa = "Extra", Regras= "2 unidades", Url = "http://www.extra.com.br"}
+            };
+        }
+    }
+
+
+}
---
+++ RealPromo/RealPromo/Models/Promocao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealPromo.Models
+{
+    public class Promocao
+    {
+        public string NomeEmpresa { get; set; }
+        public string Chamada { get; set; }
+        public string Regras { get; set; }
+        public string Url { get; set; }
+    }
+}
